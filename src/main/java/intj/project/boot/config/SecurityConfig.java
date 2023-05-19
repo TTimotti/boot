2df3b4cdf95218fc4e068ceba7c41ca6d9bcad3d
TTimotti <ttimotti@kakao.com>
2023-05-19 16:45:33 +0900

    스프링 시큐리티 설정
    - 페이지 접근시 인증여부 설정
    - 로그인 프로세스 설정
        - 향후에 성공/실패 핸들러
    - BCrypt Encoder 빈 등록

---
+++ src/main/java/intj/project/boot/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package intj.project.boot.config;
+
+import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request ->
+                        request
+                                .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                                .requestMatchers("/", "/assets/**", "/user/signIn", "/user/signUp", "/css/**", "/js/**", "/images/**", "/post/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(login ->
+                            login
+                                    .loginPage("/user/signIn") // 커스텀 로그인 페이지 지정
+                                    .loginProcessingUrl("/login") // submit 받을 URL
+                                    .usernameParameter("userId") // username를 대체할 이름 설정
+                                    .defaultSuccessUrl("/", true) // 성공시 이동할 URL
+                                    .failureUrl("/user/signIn?ERR") // 추후에 핸들러로 모달
+                                    .permitAll()
+
+                )
+                .logout(logout ->
+                            logout
+                                    .logoutUrl("/logout")
+                                    .logoutSuccessUrl("/")
+
+                );
+
+        return http.build();
+    }
+
+    //passwordEncoder
+    @Bean
+    BCryptPasswordEncoder encodePassword() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
