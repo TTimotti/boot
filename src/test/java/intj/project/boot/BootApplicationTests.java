e4e984821963e8c844dcdc219c0de7c633e27052
TTimotti <ttimotti@kakao.com>
2023-05-19 15:47:45 +0900

    Initial commit

---
+++ src/test/java/intj/project/boot/BootApplicationTests.java
@@ -0,0 +1,13 @@
+package intj.project.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
