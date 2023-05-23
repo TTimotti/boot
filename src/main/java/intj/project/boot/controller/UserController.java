7e17a4d5ffac3e3e221aa1ed9c72a985e6296154
TTimotti <ttimotti@kakao.com>
2023-05-23 11:28:03 +0900

    Header 제작
    - 픈트 어썸 추가
    - 부트스트랩 아이콘 추가
    - 부트스트랩 CSS, JS 용 map 파일 추가
    - 헤더 디자인 업데이트

---
+++ src/main/java/intj/project/boot/controller/UserController.java
@@ -0,0 +1,19 @@
+package intj.project.boot.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping(value = "/user")
+public class UserController {
+    @GetMapping(value = "/insert")
+    public String bbs() {
+
+        return "/user/insert";
+    }
+}
