61972e3bf2d7adea738263aac87b6faaf2cdbc77
TTimotti <ttimotti@kakao.com>
2023-05-23 16:18:36 +0900

    Sign Up Modal 제작중

---
+++ src/main/resources/static/scripts/common.modals.js
@@ -0,0 +1,9 @@
+const modals = {
+    showSignUpModal : function () {
+        const modal = new bootstrap.Modal('#signUpModal', {"backdrop":'static', "focus":true, "keyboard":true});
+        modal.show();
+    },
+    showSignInModal : function () {
+
+    }
+}
